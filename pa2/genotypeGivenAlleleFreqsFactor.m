75baaf7d79a2dc8a339cd4a6e1c8a8c03f78ed82
AIME15 <eugenech@mit.edu>
2015-12-24 23:45:31 -0800

    finished genotypeGivenAlleleFreqsFactor

--- pa2/genotypeGivenAlleleFreqsFactor.m
+++ pa2/genotypeGivenAlleleFreqsFactor.m
@@ -57,9 +57,16 @@ numAlleles = length(alleleFreqs);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Fill in genotypeFactor.var.  This should be a 1-D row vector.
+genotypeFactor.var = [genotypeVar];
 % Fill in genotypeFactor.card.  This should be a 1-D row vector.
-
+genotypeFactor.card = [(numAlleles * (numAlleles - 1)) / 2 + numAlleles];
 genotypeFactor.val = zeros(1, prod(genotypeFactor.card));
 % Replace the zeros in genotypeFactor.val with the correct values.
-
+for index = 1:prod(genotypeFactor.card)
+   alleles = genotypesToAlleles(index, :);
+   genotypeFactor.val(index) = prod(alleleFreqs(alleles));
+   if (alleles(1) ~= alleles(2))
+      genotypeFactor.val(index) = genotypeFactor.val(index) * 2; 
+   end
+end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  
